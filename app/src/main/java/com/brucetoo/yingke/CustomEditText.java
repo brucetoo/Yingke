0f46079ceadd949cb865b94eed213155293d6d19
brucetoo <brucetoo14@gmail.com>
2016-02-26 13:31:34 +0800

    布局对返回键的监听以及软键盘的显示和隐藏逻辑

---
+++ app/src/main/java/com/brucetoo/yingke/CustomEditText.java
@@ -0,0 +1,54 @@
+package com.brucetoo.yingke;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+/**
+ * Created by Bruce too
+ * On 2016/2/26
+ * At 13:04
+ */
+public class CustomEditText extends EditText {
+
+    private OnKeycodeBackListener listener;
+
+    public CustomEditText(Context context) {
+        super(context);
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void setKeycodeBackListener(OnKeycodeBackListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if(null != listener) {
+                listener.onKeycodeBack();
+                return true;
+            }
+        }
+        return super.onKeyPreIme(keyCode, event);
+    }
+
+    public interface OnKeycodeBackListener {
+        void onKeycodeBack();
+    }
+}
--- app/src/main/java/com/brucetoo/yingke/MainDialogFragment.java
+++ app/src/main/java/com/brucetoo/yingke/MainDialogFragment.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 
 /**
  * Created by Bruce Too
@@ -21,58 +21,89 @@ import android.widget.EditText;
 public class MainDialogFragment extends DialogFragment {
 
 
+    View rootView;
     View topHeader;
     View leftHeader;
     View btnShow;
-    EditText editInput;
-    View  activityRootView;
+    CustomEditText editInput;
+    View activityRootView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        rootView = view.findViewById(R.id.root_view);
         topHeader = view.findViewById(R.id.tv_header_top);
         leftHeader = view.findViewById(R.id.tv_header_left);
         btnShow = view.findViewById(R.id.btn_show_edit);
-        editInput = (EditText) view.findViewById(R.id.edit_input);
+        editInput = (CustomEditText) view.findViewById(R.id.edit_input);
+
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (editInput.getVisibility() == View.VISIBLE) {
-                    toggleSoftInput();
-                }
-            }
-        });
-
-        activityRootView = ((MainActivity)getActivity()).rootView;
-
-        //监听软键盘的隐藏和显示
-        activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                int heightDiff = activityRootView.getRootView().getHeight() - activityRootView.getHeight();
-                if (heightDiff > 100) { // 软键盘存在
-                    btnShow.setVisibility(View.GONE);
-                    editInput.setVisibility(View.VISIBLE);
-                    editInput.requestFocus();
-                }else {
                     btnShow.setVisibility(View.VISIBLE);
                     editInput.setVisibility(View.GONE);
+                    hideKeyboard();
                 }
             }
         });
+
+
+        //监听软键盘的隐藏和显示  但是activity的配置必须是 android:windowSoftInputMode="adjustResize" 但是此处的需求不能resize activity中的布局
+        //所有排除此方法
+//        activityRootView = ((MainActivity)getActivity()).rootView;
+//        activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @Override
+//            public void onGlobalLayout() {
+//                int heightDiff = activityRootView.getRootView().getHeight() - activityRootView.getHeight();
+//                if (heightDiff > 100) { // 软键盘存在
+//                }else {
+//                }
+//            }
+//        });
+
+
+        //这个的目的是监听软键盘显示时 点击系统的返回键目的
+        editInput.setKeycodeBackListener(new CustomEditText.OnKeycodeBackListener() {
+            @Override
+            public void onKeycodeBack() {
+                hideKeyboard();
+                //这里为了更友好 可以 用Handler延迟一段时间显示btnShow按钮 或者布局
+                btnShow.setVisibility(View.VISIBLE);
+                editInput.setVisibility(View.GONE);
+            }
+        });
+
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //点击显示软键盘和布局的逻辑
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toggleSoftInput();
+                btnShow.setVisibility(View.GONE);
+                editInput.setVisibility(View.VISIBLE);
+                editInput.requestFocus();
+                //必须在editInput显示后调用
+                showKeyboard();
             }
         });
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager)
+                getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editInput.getWindowToken(), 0);
+    }
+
+    private void showKeyboard() {
+        InputMethodManager imm = (InputMethodManager)
+                getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editInput, InputMethodManager.SHOW_FORCED);
     }
 
     public void toggleSoftInput() {
@@ -86,6 +117,13 @@ public class MainDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Dialog dialog = new Dialog(getActivity(), R.style.MainDialog);
         return dialog;
+    }
+
+    public static int getDeviceHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics mDisplayMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(mDisplayMetrics);
+        return mDisplayMetrics.heightPixels;
     }
 
 }
