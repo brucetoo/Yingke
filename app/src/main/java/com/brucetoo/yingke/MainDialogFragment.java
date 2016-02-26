f773c14c1e98de308bd10a6a9be2a9820117a148
brucetoo <brucetoo14@gmail.com>
2016-02-26 13:34:44 +0800

    监听软键盘不存在时 dialog的返回键监听 处理自定义逻辑

--- app/src/main/java/com/brucetoo/yingke/MainDialogFragment.java
+++ app/src/main/java/com/brucetoo/yingke/MainDialogFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -115,7 +116,14 @@ public class MainDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog dialog = new Dialog(getActivity(), R.style.MainDialog);
+        Dialog dialog = new Dialog(getActivity(), R.style.MainDialog){
+            @Override
+            public void onBackPressed() {
+                //监听软键盘不存在时 dialog的返回键监听 处理自定义逻辑
+                Log.e("MainDialogFragment","onBackPressed");
+                super.onBackPressed();
+            }
+        };
         return dialog;
     }
 
